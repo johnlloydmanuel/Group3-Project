13fe52d7019628ff66cbe443a4b75db3fa558679
DenverDeoGorospe <gorospedenverdeo@gmail.com>
2024-04-13 22:58:28 +0800

    PDF file,download,css

--- functions/admin/add-admin.php
+++ functions/admin/add-admin.php
@@ -2,19 +2,30 @@
 include('../functions/connection/dbconn.php');
 
 if(isset($_POST['submit']) && isset($_POST['action']) && $_POST['action'] === 'add') {
+    // Your existing form data retrieval
     $title = $_POST['title'];
     $author = $_POST['author'];
     $date_pub = $_POST['date_pub'];
     $abstract = $_POST['abstract'];
     $status = 1;
 
-    $sql = "INSERT INTO tblcapstone (title, author, date_published, abstract, is_status) VALUES (:title, :author, :date_pub, :abstract ,:is_status)";
+    // File upload handling
+    $file_name = $_FILES['pdf_file']['name'];
+    $file_tmp = $_FILES['pdf_file']['tmp_name'];
+    $file_destination = '../uploads/' . $file_name;
+
+    move_uploaded_file($file_tmp, $file_destination);
+
+    // Insert data into database
+    $sql = "INSERT INTO tblcapstone (title, author, date_published, abstract, pdf_file, is_status) 
+            VALUES (:title, :author, :date_pub, :abstract, :pdf_file, :is_status)";
 
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':title', $title);
     $stmt->bindParam(':author', $author);
     $stmt->bindParam(':date_pub', $date_pub);
     $stmt->bindParam(':abstract', $abstract);
+    $stmt->bindParam(':pdf_file', $file_destination); // Store the file path
     $stmt->bindParam(':is_status', $status);
 
     try {
--- functions/type.php
+++ functions/type.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="bootstrap.min.css">
-
 <?php
 include('../functions/connection/dbconn.php');
 include('../functions/admin/get-admin.php');
@@ -10,32 +8,17 @@ $todate = $_POST['to_date'] ?? null;
 $searchVal = $_POST['forSearch'] ?? null;
 
 if($searchVal == null && $fromdate == null && $todate == null){
-    $stmt = $conn->prepare("SELECT * FROM tblcapstone");
+    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE is_status = '1'");
 }else if($fromdate == null && $todate == null){
     $stmt = $conn->prepare("SELECT * FROM tblcapstone
-WHERE title LIKE '%$searchVal%'
-");
+WHERE title LIKE '%$searchVal%' AND is_status = '1'");
 }else if($searchVal != null && $fromdate != null && $todate != null){
     $stmt = $conn->prepare("SELECT * FROM tblcapstone
-WHERE date_published BETWEEN '$fromdate' AND '$todate' AND title LIKE '%$searchVal%'
-");
+WHERE date_published BETWEEN '$fromdate' AND '$todate' AND title LIKE '%$searchVal%' AND is_status = '1'");
 }else{
     $stmt = $conn->prepare("SELECT * FROM tblcapstone
-WHERE date_published BETWEEN '$fromdate' AND '$todate'
-");
+WHERE date_published BETWEEN '$fromdate' AND '$todate' AND is_status = '1'");
 }
 $stmt->execute();
 $searchCapstone = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
-
-
-
-
-
-
-                            
-                        
-
-
-
