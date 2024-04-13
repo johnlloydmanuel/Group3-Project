a7d88f455fa97ee24f11c1ee5bf93d187d9800ec
Temps Light <markangelo.b.deguzman@isu.edu.ph>
2024-04-13 13:09:26 +0800

    sorted files ps. paki import ung sa home-admin

--- pages/home-admin.php
+++ pages/home-admin.php
@@ -1,10 +1,10 @@
 <?php
-include('dbconn.php');
-include('get-admin.php');
-include('add-admin.php');
-include('edit-admin.php');
-include('delete-admin.php');
-include('type.php');
+include('../functions/connection/dbconn.php');
+include('../functions/admin/get-admin.php');
+include('../functions/admin/add-admin.php');
+include('../functions/admin/edit-admin.php');
+include('../functions/admin/delete-admin.php');
+include('../functions/admin/type.php');
 ?>
 
 
@@ -14,7 +14,7 @@ include('type.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
